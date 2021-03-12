d7a560f437c29c5565fa8543b94110bbf76b59f7
Mayen Akpan <neroxma@yahoo.com>
2021-03-11 19:50:40 -0800

    Implemented sockets

--- src/components/Chat/hooks/socketConnect.js
+++ src/components/Chat/hooks/socketConnect.js
@@ -1,13 +1,40 @@
 import { useEffect } from "react";
 import socketIOClient from "socket.io-client";
+import {
+  fetchChats,
+  onlineFriends,
+  onlineFriend,
+  offlineFriend,
+} from "../../../store/actions/chat";
 
 function useSocket(user, dispatch) {
   useEffect(() => {
-    // const socket = socketIOClient.connect("http://localhost:8000");
-    // socket.emit("join", user);
-    // socket.on("typing", (user) => {
-    //   console.log("Events", user);
-    // });
+    dispatch(fetchChats())
+      .then((res) => {
+        const socket = socketIOClient.connect("http://localhost:8000");
+        socket.emit("join", user);
+
+        socket.on("typing", (user) => {
+          console.log("Events", user);
+        });
+
+        socket.on("friends", (friends) => {
+          console.log("Friends", friends);
+          dispatch(onlineFriends(friends));
+        });
+
+        socket.on("online", (user) => {
+          dispatch(onlineFriend(user));
+          console.log("Online", user);
+        });
+
+        socket.on("offline", (user) => {
+          dispatch(offlineFriend(user));
+          console.log("Offline", user);
+        });
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   }, [dispatch]);
 }
 
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -5,9 +5,9 @@ const initialState = {
   // token: localStorage.getItem('token') || '',
   // isLoggedIn: !!localStorage.getItem('user'),
 
-  user: JSON.parse(localStorage.getItem("user")) || {},
-  token: localStorage.getItem("token") || "",
-  isLoggedIn: localStorage.getItem("user") ? true : false,
+  user: {},
+  token: "",
+  isLoggedIn: false,
 };
 
 const authReducer = (state = initialState, action) => {
